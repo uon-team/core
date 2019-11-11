08ef33535ba333bceda6881d5c822eba4bcf10db
Gab <gab@uon.io>
2019-11-11 13:39:13 -0500

    Changed handling of injectors in the application. A module's provider list is now scoped to module. Everything in a module's declaration list must use the module's injector

--- src/Application.ts
+++ src/Application.ts
@@ -11,6 +11,8 @@ import { Provider } from './Provider';
 // the token to use for async initialization
 export const APP_INITIALIZER = new InjectionToken<any>("Application async initializers");
 
+
+declare var console: any;
 /**
  * The application class is used to start all dependent 
  * modules and their providers
@@ -21,13 +23,14 @@ export class Application {
     private mainModuleClass: Type<any>;
 
     /// the loaded modules map
-    private _modules: Map<Type<any>, ModuleRef<any>> = new Map();
+    //private _modules: Map<Type<any>, ModuleRef<any>> = new Map();
+    private _modules: ModuleRef<any>[] = [];
 
     /// the primary (root) injector
     private _injector: Injector;
 
-    // flattened declarations
-    private _declarations: any[] = [];
+    // declarations associations with ModuleRef
+    private _declarations: Map<Type<any>, ModuleRef<any>> = new Map();
 
 
     /**
@@ -39,23 +42,20 @@ export class Application {
         this.mainModuleClass = startup;
 
         // the root provider list, start with the default providers
-        let providers: any[] = [
-            {
-                token: Application,
-                value: this
-            }
-        ];
+        let providers: any[] = [{
+            token: Application,
+            value: this
+        }];
 
-        // append all the modules' providers
-        Application.RecursivelyGetModuleProviders(startup, providers);
-
-        Application.RecursivelyGetModuleDeclarations(startup, this._declarations);
 
         // create the root injector
         this._injector = Injector.Create(providers);
 
+        // get providers form main module
+        let mod: Module = GetTypeMetadata(this.mainModuleClass).find(m => m instanceof Module);
+
         // finally load all modules
-        this.recursivelyLoadModules(this.mainModuleClass);
+        this.recursivelyLoadModules(this.mainModuleClass, this._injector, mod.providers);
 
     }
 
@@ -85,19 +85,22 @@ export class Application {
      */
     async start(): Promise<boolean> {
 
-        // get the initializer list
-        const initializers = this._injector.get(APP_INITIALIZER, []);
-
-        // chain initializers
-        for (let i = 0; i < initializers.length; ++i) {
-            await initializers[i];
-        }
-
         // instanciate modules
-        for (let [mt, ref] of this._modules) {
+        for (let i = 0; i < this._modules.length; ++i) {
+
+            let ref = this._modules[i];
+
+            const initializers = ref.injector.get(APP_INITIALIZER, []);
+
+            // chain initializers
+            for (let i = 0; i < initializers.length; ++i) {
+                await initializers[i];
+            }
+
             // create the module instance
-            let instance = ref.injector.instanciate(mt);
+            let instance = ref.injector.instanciate(ref.type);
             ref.instance = instance;
+
         }
 
         return true;
@@ -108,143 +111,130 @@ export class Application {
      * Load all modules recursively
      * @param type 
      */
-    private recursivelyLoadModules(type: Type<any> | ModuleWithProviders) {
+    private recursivelyLoadModules(type: Type<any> | ModuleWithProviders, parentInjector: Injector, initialProviders: Provider[] = []) {
 
         // get module's meta data
-
-        let module: Module = null;
         let module_type: Type<any> = (type as ModuleWithProviders).module || (type as Type<any>);
-        let annotations: any[] = GetTypeMetadata(module_type);
+        let mod: Module = GetTypeMetadata(module_type).find(m => m instanceof Module);
 
-        for (let i = 0; i < annotations.length; ++i) {
 
-            let a = annotations[i];
-            if (a instanceof Module) {
+        if (!mod) {
+            throw new Error(`${module_type} was not decoratored with @Module()`)
+        }
 
-                let mod = a as Module;
-                let module_ref = this._modules.get(module_type);
+        // create a module ref
+        const ref = <ModuleRef<any>>({
+            module: mod,
+            type: module_type
+        });
 
-                if (!module_ref) {
 
-                    const ref = <ModuleRef<any>>({
-                        module: mod,
-                        type: module_type
-                    });
+        let providers: Provider[] = initialProviders
+            .concat([{ token: ModuleRef, value: ref }]);
 
-                    // create an injector for the module
-                    const injector = Injector.Create([{ token: ModuleRef, value: ref }], this._injector);
+        // providers from imports should be available in this module
+        if (mod.imports && mod.imports.length) {
 
-                    // set the injector into module ref
-                    ref.injector = injector;
+            const loaded_imports: any[] = [];
 
-                    // assign the module
-                    this._modules.set(module_type, ref);
+            mod.imports.forEach((m) => {
 
-                    // load imported modules
-                    if (mod.imports && mod.imports.length) {
-                        mod.imports.forEach((m) => {
-                            this.recursivelyLoadModules(m);
-                        });
-                    }
+                // get module type
+                let module_type: Type<any> = (m as ModuleWithProviders).module || (m as Type<any>);
 
+                // check for duplicates
+                if (loaded_imports.indexOf(module_type) > -1) {
+                    throw new Error(`${module_type} is imported twice in the same module. If you must import this module twice, move the second occurence to another module.`)
                 }
 
-            }
+                loaded_imports.push(module_type)
+
+                // get module metadata
+                let mod: Module = GetTypeMetadata(module_type).find(m => m instanceof Module);
+
+                // get import providers
+                let extra_providers = (m as ModuleWithProviders).providers || [];
+                let import_providers = (mod.providers || []).concat(extra_providers);
+
+                // append to list
+                providers.push(...import_providers);
+
+            });
 
         }
 
-    }
+        // create an injector for the module
+        const injector = Injector.Create(providers, parentInjector);
 
-    static RecursivelyGetModuleProviders(moduleType: Type<any> | ModuleWithProviders, out: any[]) {
+        // set the injector into module ref
+        ref.injector = injector;
 
-        let module: Module = null;
-        let module_type: Type<any> = (moduleType as ModuleWithProviders).module || (moduleType as Type<any>);
-        let extra_providers: Provider[] = (moduleType as ModuleWithProviders).providers;
-        let annotations: any[] = GetTypeMetadata(module_type);
+        // add to module list
+        this._modules.push(ref);
 
-        for (let i = 0; i < annotations.length; ++i) {
-            let a = annotations[i];
-            if (a instanceof Module) {
+        if(mod.declarations && mod.declarations.length) {
 
-                let mod = a as Module;
-                let providers: Provider[] = [];
-                let imports = mod.imports;
+            for (let i = 0; i < mod.declarations.length; i++) {
+                const decl = mod.declarations[i];
 
-
-                // if other providers were declared in the module metadata
-                if (mod.providers && mod.providers.length) {
-                    providers = providers.concat(mod.providers);
+                const at_module = this._declarations.get(decl);
+                if(at_module) {
+                    throw new Error(`Cannot redeclare ${decl.name} in module ${module_type}, it is already declared in module ${at_module.type.name} `)
                 }
 
-                // if module is a ModuleWithProviders, concat the providers to the list
-                if (extra_providers) {
-                    providers = providers.concat(extra_providers);
-                }
-
-                // go thru all imports
-                if (imports && imports.length) {
-                    for (let j = 0; j < imports.length; ++j) {
-                        this.RecursivelyGetModuleProviders(imports[j], out);
-                    }
-                }
-
-                // finally add own providers
-                if (providers && providers.length) {
-                    for (let j = 0; j < providers.length; ++j) {
-                        if (out.indexOf(providers[j]) === -1) {
-                            out.push(providers[j])
-                        }
-                    }
-                }
-
+                this._declarations.set(decl, ref);
             }
-
         }
+
+
+        // load imported modules
+        if (mod.imports && mod.imports.length) {
+            mod.imports.forEach((m) => {
+                this.recursivelyLoadModules(m, injector);
+            });
+        }
+
 
     }
 
 
     static RecursivelyGetModuleDeclarations(moduleType: Type<any> | ModuleWithProviders, out: any[]) {
 
-        let module: Module = null;
+
         let module_type: Type<any> = (moduleType as ModuleWithProviders).module || (moduleType as Type<any>);
-        //let extra_providers: Provider[] = (moduleType as ModuleWithProviders).providers;
-        let annotations: any[] = GetTypeMetadata(module_type);
+        let mod: Module = GetTypeMetadata(module_type).find(m => m instanceof Module);
 
-        for (let i = 0; i < annotations.length; ++i) {
-            let a = annotations[i];
-            if (a instanceof Module) {
-
-                let mod = a as Module;
-                let declarations: Type<any>[] = [];
-                let imports = mod.imports;
-
-
-                // if other providers were declared in the module metadata
-                if (mod.declarations && mod.declarations.length) {
-                    declarations = declarations.concat(mod.declarations);
-                }
-
-
-                // go thru all imports
-                if (imports && imports.length) {
-                    for (let j = 0; j < imports.length; ++j) {
-                        this.RecursivelyGetModuleDeclarations(imports[j], out);
-                    }
-                }
-
-                // finally add own declarations
-                if (declarations && declarations.length) {
-                    for (let j = 0; j < declarations.length; ++j) {
-                        if (out.indexOf(declarations[j]) === -1) {
-                            out.push(declarations[j])
-                        }
-                    }
-                }
-
-            }
-
+        if (!mod) {
+            throw new Error(`${module_type} was not decoratored with @Module()`)
         }
+
+        let declarations: Type<any>[] = [];
+        let imports = mod.imports;
+
+
+        // if other providers were declared in the module metadata
+        if (mod.declarations && mod.declarations.length) {
+            declarations = declarations.concat(mod.declarations);
+        }
+
+
+        // go thru all imports
+        if (imports && imports.length) {
+            for (let j = 0; j < imports.length; ++j) {
+                this.RecursivelyGetModuleDeclarations(imports[j], out);
+            }
+        }
+
+        // finally add own declarations
+        if (declarations && declarations.length) {
+            for (let j = 0; j < declarations.length; ++j) {
+                if (out.indexOf(declarations[j]) === -1) {
+                    out.push(declarations[j])
+                }
+            }
+        }
+
+
 
     }
 
